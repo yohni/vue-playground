4867d7600e15a945e066cb3377ac6b76dcee020b
Achmad Yohni <yohni@Achmads-MacBook-Pro.local>
2021-02-23 07:14:07 +0700

    remove icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-
-  babel: { compact: true },
+  build: {
+    babel: { compact: true },
+  },
 }
