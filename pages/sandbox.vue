d6924628b2577e9ffa0516cd472000c1d57b1ab9
yohni <yohni.123@gmail.com>
2021-03-28 15:44:34 +0700

    conditional class

---
+++ pages/sandbox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    :class="`hakki ${isClicked ? 'hakki__clicked' : ''}`"
+    @click="updateClicked"
+  >
+    Hakki
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isClicked: false,
+    }
+  },
+  methods: {
+    updateClicked() {
+      this.isClicked = !this.isClicked
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.hakki
+  background red
+  color white
+
+  &__clicked
+    background blue
+</style>
